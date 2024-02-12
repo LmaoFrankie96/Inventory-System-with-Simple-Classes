4834e6ac889cee7018f1fc754f70358edb51f6d3
Muhammad Ramish <mramish9696@gmail.com>
2024-02-12 13:02:30 +0500

    - unnecassory comments removed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,12 @@ public class Player : MonoBehaviour
 {
     private Inventory _inventory;
     [SerializeField] private UI_Inventory _uiInventory;
-    //[SerializeField] private ItemWorld _itemWorld;
     public GameObject[] itemButtons;
     public int itemFlag;
     public GameObject inventoryFullPopUp;
     private void Start()
     {
         _inventory = new Inventory();
-        foreach (var item in _inventory.itemList)
-            Debug.Log($"Item type is: {item.itemType}");
         _uiInventory.SetInventory(_inventory);
         itemFlag = 0;
     }
@@ -27,31 +24,25 @@ public class Player : MonoBehaviour
             {
 
                 _inventory.AddItem(new Item(ItemType.HealthPotion, 1, _inventory.itemList.Count));
-                Debug.Log(_inventory.itemList[_inventory.itemList.Count - 1].itemType);
             }
             else if (itemFlag == 1)
             {
                 _inventory.AddItem(new Item(ItemType.Sword, 1, _inventory.itemList.Count));
-                Debug.Log(_inventory.itemList[_inventory.itemList.Count - 1].itemType);
 
             }
             else if (itemFlag == 2)
             {
                 _inventory.AddItem(new Item(ItemType.Coin, 1, _inventory.itemList.Count));
-                Debug.Log(_inventory.itemList[_inventory.itemList.Count - 1].itemType);
 
             }
             else if (itemFlag == 3)
             {
                 _inventory.AddItem(new Item(ItemType.Medkit, 1, _inventory.itemList.Count));
-                Debug.Log(_inventory.itemList[_inventory.itemList.Count - 1].itemType);
 
             }
-            Debug.Log($"Inventory count is: {_inventory.itemList.Count}");
         }
         else {
             inventoryFullPopUp.SetActive(true);
-            Debug.Log("Inventory is full");
         }
 
     }
@@ -62,7 +53,6 @@ public class Player : MonoBehaviour
     }
     public void RemoveItemFromInventory()
     {
-        Debug.Log("Remove item from inventory on click is being called");
         _inventory.RemoveItem(_inventory.itemList[_inventory.itemList.Count - 1]);
 
     }
--- Assets/Scripts/UI_Inventory.cs
+++ Assets/Scripts/UI_Inventory.cs
@@ -1,34 +1,25 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Inventory : MonoBehaviour
 {
     private Inventory _inventory;
-    //private List<Item> inventoryItemList;
+    
     [SerializeField] private Transform _itemSlotContainer;
     [SerializeField] private Transform _itemSlotTemplate;
     [SerializeField] private Image _itemImage;
-    //  [SerializeField] private RectTransform _slotContainerRectTransform;
+    
 
-    private void Awake()
-    {
-        //inventoryItemList = _inventory.GetItemList();
-    }
     public void SetInventory(Inventory inventory)
     {
-        //Debug.Log("I am in SetInventory function");
         this._inventory = inventory;
-        //inventoryItemList = _inventory.GetItemList();
         _inventory.OnItemListChanged += Inventory_OnItemListChanged;
         RefreshInventoryItems();
     }
 
     private void Inventory_OnItemListChanged(object sender, EventArgs e)
     {
-        //inventoryItemList = _inventory.GetItemList();
         RefreshInventoryItems();
     }
 
@@ -39,16 +30,12 @@ public class UI_Inventory : MonoBehaviour
             if (child == _itemSlotTemplate) continue;
             Destroy(child.gameObject);
         }
-        //Debug.Log("I am in RefreshInventoryItems function");
         int x = 0;
         int y = 0;
         float itemSlotCellSize = 200f;
-        //Debug.Log($"Inventory list count is: {inventoryItemList.Count}");
         foreach (Item item in _inventory.itemList)
         {
-            //RectTransform itemSlotRectTransform=Instantiate(_itemSlotTemplate,_itemSlotContainer).GetComponent<RectTransform>();
-            //Debug.Log($"I am item number: {inventoryItemList.IndexOf(item)}");
-            //Debug.Log($"I am item number: {inventoryItemList[inventoryItemList.IndexOf(item)].GetSprite().name}");
+            
             _itemImage.sprite=item.GetSprite();
             RectTransform itemSlotRectTransform = (RectTransform)Instantiate(_itemSlotTemplate, _itemSlotContainer);
             itemSlotRectTransform.gameObject.SetActive(true);
@@ -62,8 +49,5 @@ public class UI_Inventory : MonoBehaviour
         }
 
     }
-    /*public void AddItemToInventory() { 
     
-        
-    }*/
 }
